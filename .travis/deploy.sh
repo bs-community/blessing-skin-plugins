e375453bf7c678549b9c60889137b5cf713fd085
printempw <h@prinzeugen.net>
2018-02-14 17:35:27 +0800

    Fix Travis CI config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,13 +2,7 @@
 set -ev
 export TZ='Asia/Shanghai'
 
-# Avoid direct force push
-git clone git@git.coding.net:printempw/bs-plugins-archive.git .deploy_git
-
-cd .deploy_git
-git checkout master
-mv .git/ ../.dist/
-cd ../.dist
+cd .dist
 
 git add .
 git commit -m "Archive updated: `date +"%Y-%m-%d %H:%M:%S"`"
