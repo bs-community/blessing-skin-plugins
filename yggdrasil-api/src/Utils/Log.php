06dd83408dacf5188c62e19de054aabf11d8e967
printempw <h@prinzeugen.net>
2018-02-14 20:58:50 +0800

    Change log file location

--- yggdrasil-api/src/Utils/Log.php
+++ yggdrasil-api/src/Utils/Log.php
@@ -2,6 +2,8 @@
 
 namespace Yggdrasil\Utils;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 use Illuminate\Support\Facades\Facade;
 
 class Log extends Facade
@@ -18,9 +20,18 @@ class Log extends Facade
 
     public static function __callStatic($method, $args)
     {
-        // only log when verbose log is enabled
+        // 修改日志记录文件至 storage/logs/yggdrasil.log
+        $monolog = parent::__callStatic('getMonolog', []);
+        $monolog->popHandler();
+        $monolog->pushHandler(
+            (new StreamHandler(storage_path('logs/yggdrasil.log')))->setFormatter(
+                new LineFormatter(null, null, true, true)
+            )
+        );
+
+        // 仅当选项开启时记录日志
         if (option('ygg_verbose_log')) {
-            parent::__callStatic($method, $args);
+            return parent::__callStatic($method, $args);
         }
     }
 }
