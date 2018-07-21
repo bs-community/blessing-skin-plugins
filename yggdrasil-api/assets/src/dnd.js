543e3d6c895e0f4a169ac3682fe4b984f258dfd9
printempw <h@prinzeugen.net>
2018-07-21 20:56:26 +0800

    Use a element for DnD since buttons can't be draggable on FireFox
    See: https://bugzilla.mozilla.org/show_bug.cgi?id=568313

--- yggdrasil-api/assets/src/dnd.js
+++ yggdrasil-api/assets/src/dnd.js
@@ -10,11 +10,7 @@ let dom = `
       <p>点击下方按钮复制 API 地址，或者将按钮拖动至启动器的任意界面即可快速添加认证服务器（目前仅支持 HMCL 3.1.74 及以上版本）。</p>
     </div><!-- /.box-body -->
     <div class="box-footer">
-      <button id="dnd-button"
-        class="btn btn-primary"
-        draggable="true"
-        data-clipboard-text="${ url('api/yggdrasil') }"
-      >将此按钮拖动至启动器</button>
+      <a id="dnd-button" class="btn btn-primary" draggable="true" data-clipboard-text="${ url('api/yggdrasil') }">将此按钮拖动至启动器</a>
       <a class="btn" target="_blank" href="https://github.com/bs-community/yggdrasil-api/wiki/0x03-%E9%85%8D%E5%90%88-authlib-injector-%E4%BD%BF%E7%94%A8#%EF%B8%8F-%E9%85%8D%E7%BD%AE%E5%90%AF%E5%8A%A8%E5%99%A8">启动器配置教程</a>
     </div>
   </div><!-- /.box -->
