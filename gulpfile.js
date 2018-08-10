090f352215c76a9348603e3632a3377875fc2b29
printempw <h@prinzeugen.net>
2018-08-10 13:03:58 +0800

    Add gulp script "publish"
    Automatically generate "plugins.json" containing metadata of all plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const gulp     = require('gulp'),
-      fs       = require('fs'),
-      path     = require('path'),
-      chalk    = require('chalk'),
-      merge    = require('merge-stream'),
-      zip      = require('gulp-zip'),
-      execSync = require('child_process').execSync;
+const
+    chalk    = require('chalk'),
+    crypto   = require('crypto'),
+    execSync = require('child_process').execSync,
+    fs       = require('fs'),
+    gulp     = require('gulp'),
+    merge    = require('merge-stream'),
+    path     = require('path'),
+    yaml     = require('js-yaml'),
+    zip      = require('gulp-zip');
 
 const distPath    = '.dist';
 const pluginsPath = './';
-const excludePath = [
-    distPath,
-    '.git',
-    '.travis',
-    'node_modules'
-];
+const excludePath = [distPath, '.git', '.travis', 'node_modules'];
+const archiveRepoBaseUrl = 'https://coding.net/u/printempw/p/bs-plugins-archive/git/raw/master/';
+const yamlTranslationTemp = {};
 
 function getPluginFolders(dir) {
     return fs.readdirSync(dir).filter(
@@ -99,3 +99,87 @@ gulp.task('build', () => {
         }
     });
 });
+
+gulp.task('publish', () => {
+    let packages = [];
+
+    getPluginFolders(pluginsPath).map(folder => {
+        let packageInfo = require(`./${folder}/package.json`);
+        const keeps = ['name', 'version', 'title', 'description', 'author', 'require'];
+
+        // Remove unnecessary fields
+        for (const key in packageInfo) {
+            if (! keeps.includes(key)) {
+                delete packageInfo[key];
+            }
+        }
+
+        // Parse i18n plugin title & description
+        if (packageInfo.title.includes('::')) {
+            packageInfo.title = trans(folder, packageInfo.title, 'zh_CN');
+        }
+
+        if (packageInfo.description.includes('::')) {
+            packageInfo.description = trans(folder, packageInfo.description, 'zh_CN');
+        }
+
+        let archiveFileName = `${folder}_v${packageInfo.version}.zip`;
+        let shasum = crypto.createHash('sha1').update(
+            fs.readFileSync(path.join(distPath, archiveFileName)
+        )).digest('hex');
+
+        packageInfo['dist'] = {
+            type: 'zip',
+            url: archiveRepoBaseUrl + archiveFileName,
+            shasum: shasum
+        };
+
+        packages.push(packageInfo);
+    });
+
+    const pluginsJson = {
+        "//": [
+            "This file provides metadata of all plugins available for Blessing Skin",
+            "Publish your plugin at https://github.com/bs-community/blessing-skin-plugins",
+            "This file is @generated automatically"
+        ],
+        "packages": packages
+    };
+
+    const pluginsJsonPath = path.join(distPath, 'plugins.json');
+    fs.writeFileSync(pluginsJsonPath, JSON.stringify(pluginsJson, null, '  '), 'utf8');
+
+    console.log(`Plugins metadata updated: ${ chalk.underline.green(pluginsJsonPath) }`);
+});
+
+function trans(plugin, key, locale) {
+    const realKey = key.split('::')[1];
+    const yamlPath = `./${plugin}/lang/${locale}/${realKey.split('.')[0]}.yml`;
+
+    let temp;
+
+    try {
+        // Load translation strings from YAML file
+        if (yamlTranslationTemp[yamlPath] !== undefined) {
+            temp = yamlTranslationTemp[yamlPath];
+        } else {
+            temp = yaml.safeLoad(fs.readFileSync(yamlPath, 'utf8'));
+            yamlTranslationTemp[yamlPath] = temp;
+        }
+    } catch (e) {
+        console.error(e);
+        return key;
+    }
+
+    const segments = realKey.split('.').slice(1);
+
+    for (const i in segments) {
+        if (temp[segments[i]] === undefined) {
+            return key;
+        } else {
+            temp = temp[segments[i]];
+        }
+    }
+
+    return temp;
+}
