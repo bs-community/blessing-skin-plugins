8879b5b56984c44230e630f8f6ceb531d5c1cc0e
Pig Fang <g-plane@hotmail.com>
2018-09-08 21:26:01 +0800

    Simplify using hook

--- invitation-codes/assets/register.js
+++ invitation-codes/assets/register.js
@@ -1,13 +1,13 @@
 'use strict';
 
-window.bsEmitter.on('mounted', ({ el }) => setTimeout(() => {
+window.bsEmitter.on('mounted', ({ el }) => {
     $(el).find('.row').first().before(
         `<div class="form-group has-feedback">
             <input id="invitation-code" type="text" class="form-control" placeholder="邀请码">
             <span class="glyphicon glyphicon-inbox form-control-feedback"></span>
         </div>`
     );
-}, 100));
+});
 
 // 插入邀请码的值
 window.bsEmitter.on('beforeFetch', request => {
