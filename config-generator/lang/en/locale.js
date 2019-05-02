6b99d47c5be9603d8b0b0866512a6d6cff7aa44e
Pig Fang <g-plane@hotmail.com>
2019-05-02 14:50:50 +0800

    [config-generator] fix injecting i18n

--- config-generator/lang/en/locale.js
+++ config-generator/lang/en/locale.js
@@ -1,7 +1,9 @@
-blessing.i18n.configGenerator = {
-  csl13_1Upper: 'v13.1 and upper (recommended)',
-  csl13_1Lower: 'lower than v13.1',
-  usm1_4Upper: 'v1.4 and upper (recommended)',
-  usm1_2To1_3: 'v1.2 to v1.3',
-  usm1_2Lower: 'lower than v1.2'
-}
+blessing.event.on('i18nLoaded', i18n => {
+  i18n.configGenerator = {
+    csl13_1Upper: 'v13.1 and upper (recommended)',
+    csl13_1Lower: 'lower than v13.1',
+    usm1_4Upper: 'v1.4 and upper (recommended)',
+    usm1_2To1_3: 'v1.2 to v1.3',
+    usm1_2Lower: 'lower than v1.2'
+  }
+})
