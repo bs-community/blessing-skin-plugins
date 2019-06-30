d36c6c8978444789eb9627fc86a6215d36f5067d
Asnxthaony <asnxthaony@outlook.com>
2019-07-01 01:32:56 +0800

    [hitokoto] Use another way to get the hitokoto

--- hitokoto/bootstrap.php
+++ hitokoto/bootstrap.php
@@ -5,19 +5,23 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
 
     $events->listen(App\Events\RenderingFooter::class, function($event) {
-        // Get Hitokoto
-	    try {
-            $hitokoto = file_get_contents("https://v1.hitokoto.cn/?encode=text");
-        } catch(Exception $e) {
-            $hitokoto = "";
-        }
         $content = <<< EOT
 <script>
 if ($('.breadcrumb').length > 0) {
-    $('.breadcrumb').append("<p class='hitokoto'>$hitokoto</p>");
+    $('.breadcrumb').append("<p class='hitokoto'></p>");
 } else {
-    $('.content-header').append("<div class='breadcrumb'><p class='hitokoto'>$hitokoto</p></div>");
+    $('.content-header').append("<div class='breadcrumb'><p class='hitokoto'></p></div>");
 }
+
+fetch("https://v1.hitokoto.cn?encode=json").then(function(response) {
+    return response.json();
+}).then(function(data) {
+    $('.hitokoto').text(data.hitokoto);
+}).
+catch(function(err) {
+
+});
+
 </script>
 EOT;
 
@@ -30,4 +34,3 @@ EOT;
     });
 
 };
-
