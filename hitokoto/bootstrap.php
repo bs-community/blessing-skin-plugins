e4fc8850674965f0528500fc06cb31909ce202e9
Asnxthaony <asnxthaony@outlook.com>
2019-07-01 18:15:00 +0800

    Update bootstrap.php

--- hitokoto/bootstrap.php
+++ hitokoto/bootstrap.php
@@ -1,34 +1,18 @@
 <?php
 
+use App\Services\Hook;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
 
-    $events->listen(App\Events\RenderingFooter::class, function($event) {
-        $content = <<< EOT
-<script>
-if ($('.breadcrumb').length > 0) {
-    $('.breadcrumb').append("<p class='hitokoto'></p>");
-} else {
-    $('.content-header').append("<div class='breadcrumb'><p class='hitokoto'></p></div>");
-}
-
-fetch("https://v1.hitokoto.cn?encode=json").then(function(response) {
-    return response.json();
-}).then(function(data) {
-    $('.hitokoto').text(data.hitokoto);
-}).
-catch(function(err) {
-});
-</script>
-EOT;
-
-        $event->addContent($content);
-    });
+    Hook::addScriptFileToPage(
+        plugin_assets('hitokoto', 'hitokoto.js'),
+    );
 
     $events->listen(App\Events\RenderingHeader::class, function($event) {
         // We need some CSS to position the paragraph
         $event->addContent('<style> .hitokoto { display: inline; margin-left: 15px; } </style>');
     });
+
 
 };
