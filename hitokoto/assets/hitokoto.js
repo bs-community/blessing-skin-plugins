6b9807ecd89ea40da897e0917782abc091fe2883
Pig Fang <g-plane@hotmail.com>
2019-07-01 21:31:02 +0800

    [hitokoto] run prettier

--- hitokoto/assets/hitokoto.js
+++ hitokoto/assets/hitokoto.js
@@ -1,14 +1,13 @@
 if (document.querySelectorAll('.breadcrumb').length > 0) {
-   document.querySelector('.breadcrumb').innerHTML += "<p class='hitokoto'></p>";
+  document.querySelector('.breadcrumb').innerHTML += '<p class="hitokoto"></p>'
 } else {
-    document.querySelector('.content-header').innerHTML += "<div class='breadcrumb'><p class='hitokoto'></p></div>";
+  document.querySelector('.content-header').innerHTML +=
+    '<div class="breadcrumb"><p class="hitokoto"></p></div>'
 }
 
-fetch("https://v1.hitokoto.cn?encode=text").then((response) => {
-    return response.text();
-}).then((data) => {
+fetch('https://v1.hitokoto.cn?encode=text')
+  .then(response => response.text())
+  .then(data => {
     document.querySelector('.hitokoto').textContent = data
-}).
-catch((err) => {
-
-});
+  })
+  .catch(err => {})
