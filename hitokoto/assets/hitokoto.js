205eef7ad25f1fb2d02205b3f9a6a3865e773536
Asnxthaony <asnxthaony@outlook.com>
2019-07-01 20:33:52 +0800

    [hitokoto] Use another way to get the hitokoto (#29)
    * [hitokoto] Use another way to get the hitokoto
    
    * Remove empty line
    
    * Update bootstrap.php
    
    * Create hitokoto.js
    
    * [hitokoto] Remove empty line
    
    * Update hitokoto/assets/hitokoto.js
    
    Co-Authored-By: Pig Fang <g-plane@hotmail.com>
    
    * Update hitokoto.js
    
    * Update hitokoto.js

---
+++ hitokoto/assets/hitokoto.js
@@ -0,0 +1,14 @@
+if (document.querySelectorAll('.breadcrumb').length > 0) {
+   document.querySelector('.breadcrumb').innerHTML += "<p class='hitokoto'></p>";
+} else {
+    document.querySelector('.content-header').innerHTML += "<div class='breadcrumb'><p class='hitokoto'></p></div>";
+}
+
+fetch("https://v1.hitokoto.cn?encode=text").then((response) => {
+    return response.text();
+}).then((data) => {
+    document.querySelector('.hitokoto').textContent = data
+}).
+catch((err) => {
+
+});
--- hitokoto/bootstrap.php
+++ hitokoto/bootstrap.php
@@ -1,28 +1,13 @@
 <?php
 
+use App\Services\Hook;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
 
-    $events->listen(App\Events\RenderingFooter::class, function($event) {
-        // Get Hitokoto
-	    try {
-            $hitokoto = file_get_contents("https://v1.hitokoto.cn/?encode=text");
-        } catch(Exception $e) {
-            $hitokoto = "";
-        }
-        $content = <<< EOT
-<script>
-if ($('.breadcrumb').length > 0) {
-    $('.breadcrumb').append("<p class='hitokoto'>$hitokoto</p>");
-} else {
-    $('.content-header').append("<div class='breadcrumb'><p class='hitokoto'>$hitokoto</p></div>");
-}
-</script>
-EOT;
-
-        $event->addContent($content);
-    });
+    Hook::addScriptFileToPage(
+        plugin_assets('hitokoto', 'hitokoto.js'),
+    );
 
     $events->listen(App\Events\RenderingHeader::class, function($event) {
         // We need some CSS to position the paragraph
@@ -30,4 +15,3 @@ EOT;
     });
 
 };
-
