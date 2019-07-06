8cfda791c4c23dc3bf00c97107e4cc11874c0e97
Pig Fang <g-plane@hotmail.com>
2019-07-06 10:41:34 +0800

    [forum-integration] change storing db config

--- forum-integration/bootstrap.php
+++ forum-integration/bootstrap.php
@@ -6,20 +6,44 @@ use Integration\Forum\Listener;
 require __DIR__.'/src/helpers.php';
 
 return function () {
+    $oldConfig = option('forum_db_config', '');
+    if ($oldConfig) {
+        $oldConfig = (array) @unserialize($oldConfig);
+        option([
+            'forum_db_host' => Arr::get($oldConfig, 'host'),
+            'forum_db_port' => Arr::get($oldConfig, 'port'),
+            'forum_db_database' => Arr::get($oldConfig, 'database'),
+            'forum_db_username' => Arr::get($oldConfig, 'username'),
+            'forum_db_password' => Arr::get($oldConfig, 'password'),
+            'forum_db_table' => Arr::get($oldConfig, 'table'),
+        ]);
+        DB::table('options')->where('option_name', 'forum_db_config')->delete();
+    }
+
     // 绑定 Query Builder 至容器，方便之后直接调用
     App::instance('db.local', DB::connection()->table('users'));
     App::singleton('db.remote', function () {
-        $config = @unserialize(option('forum_db_config', ''));
+        $config = [
+            'host' => option('forum_db_host', '127.0.0.1'),
+            'port' => option('forum_db_port', 3306),
+            'database' => option('forum_db_database', 'forum'),
+            'username' => option('forum_db_username', 'default'),
+            'password' => option('forum_db_password', 'secret'),
+            'table' => option('forum_db_table', 'users'),
+        ];
 
         config(['database.connections.remote' => array_merge(
-            forum_get_default_db_config(), (array) $config
+            forum_get_default_db_config(), $config
         )]);
 
-        return DB::connection('remote')->table(Arr::get($config, 'table'));
+        return DB::connection('remote')->table($config['table']);
     });
 
     try {
         app('db.remote')->getConnection()->getPdo();
+        if (! Schema::connection('remote')->hasTable($config['table'])) {
+            return;
+        }
     } catch (Exception $e) {
         // 目标数据库没配置好之前啥也不干
         return;
--- forum-integration/views/config.blade.php
+++ forum-integration/views/config.blade.php
@@ -7,12 +7,12 @@
 <?php
   $targetDbConfigForm = Option::form('connection', '数据库连接配置', function ($form) {
 
-    $form->text('forum_db_config[host]',     '目标数据库地址')->hint('跨数据库主机进行对接可能会有延迟，敬请知悉。');
-    $form->text('forum_db_config[port]',     '端口');
-    $form->text('forum_db_config[database]', '数据库名');
-    $form->text('forum_db_config[username]', '用户名');
-    $form->text('forum_db_config[password]', '密码');
-    $form->text('forum_db_config[table]',    '用户数据表名');
+    $form->text('forum_db_host',     '目标数据库地址')->hint('跨数据库主机进行对接可能会有延迟，敬请知悉。');
+    $form->text('forum_db_port',     '端口');
+    $form->text('forum_db_database', '数据库名');
+    $form->text('forum_db_username', '用户名');
+    $form->text('forum_db_password', '密码');
+    $form->text('forum_db_table',    '用户数据表名');
 
     $form->select('forum_duplicated_prefer', '重复处理')
       ->option('remote', '用论坛程序上的用户数据覆盖皮肤站')
@@ -20,15 +20,17 @@
       ->description('此项选择后，在用户数据（如用户名相同、用户名密码不同）冲突的情况下将以你选择的那一方为准，另一方的用户数据将被覆盖');
 
   })->handle()->always(function ($form) {
-    $config = request('forum_db_config');
-    if ($config) {
-      option(['forum_db_config' => serialize($config)]);
-    } else {
-      $config = @unserialize(option('forum_db_config'));
-    }
+    $config = [
+        'host' => option('forum_db_host', '127.0.0.1'),
+        'port' => option('forum_db_port', 3306),
+        'database' => option('forum_db_database', 'forum'),
+        'username' => option('forum_db_username', 'default'),
+        'password' => option('forum_db_password', 'secret'),
+        'table' => option('forum_db_table', 'users'),
+    ];
 
     config(['database.connections.remote' => array_merge(
-      forum_get_default_db_config(), (array) $config
+      forum_get_default_db_config(), $config
     )]);
 
     try {
@@ -62,7 +64,7 @@
       <div class="col-md-6">
         <div class="box">
           <div class="box-header with-border">
-            <h3 class="box-title">README.md</h3>
+            <h3 class="box-title">说明</h3>
           </div><!-- /.box-header -->
           <div class="box-body table-responsive">
             <?php
