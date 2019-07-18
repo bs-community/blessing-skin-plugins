34424e8c84d053e515c91e942c6e6bc7795b6fe5
Pig Fang <g-plane@hotmail.com>
2019-07-18 22:05:23 +0800

    fix

--- pray-for-kyoto-animation/bootstrap.php
+++ pray-for-kyoto-animation/bootstrap.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->listen(RenderingHeader::class, function ($event) {
         $path = request()->path();
-        $excludes = ['user/player', 'user/closet', 'skinlib', 'skinlib/*'];
+        $excludes = ['user/player', 'user/closet', 'skinlib', 'skinlib/show/*', 'skinlib/*'];
         if (! in_array($path, $excludes)) {
             $event->addContent('<style>html { filter: gray; -webkit-filter: grayscale(100%); }</style>');
         }
