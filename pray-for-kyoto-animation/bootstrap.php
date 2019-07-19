a677bca3bd206ac3f2a8076a427aab37aa678408
Little_Qiu <littleqiu@littleservice.cn>
2019-07-20 06:33:13 +0800

    [pray-for-kyoto-animation] Fix the texture details (#33)
    * [pray-for-kyoto-animation] Fix the texture details
    
    修复皮肤库的上传材质和材质详情页面仍然会应用滤镜的问题。
    
    * [pray-for-kyoto-animation] Bump version

--- pray-for-kyoto-animation/bootstrap.php
+++ pray-for-kyoto-animation/bootstrap.php
@@ -7,8 +7,8 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->listen(RenderingHeader::class, function ($event) {
         $path = request()->path();
-        $excludes = ['user/player', 'user/closet', 'skinlib', 'skinlib/show/*', 'skinlib/*'];
-        if (! in_array($path, $excludes)) {
+        $excludes = ['user/player', 'user/closet', 'skinlib'];
+        if (! (in_array($path, $excludes) || explode("/", $path, 2)[0] == 'skinlib')) {
             $event->addContent('<style>html { filter: gray; -webkit-filter: grayscale(100%); }</style>');
         }
     });
