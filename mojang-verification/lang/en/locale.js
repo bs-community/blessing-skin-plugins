6cfb8814d831e31c241201f3a5cd00d172acd368
Little_Qiu <littleqiu@littleservice.cn>
2019-08-31 21:38:30 +0800

    [mojang-verification] Update lang/en/locale.js

--- mojang-verification/lang/en/locale.js
+++ mojang-verification/lang/en/locale.js
@@ -3,9 +3,9 @@ blessing.i18n.mojang_verification = {
     bind: {
         title: 'Bind your Mojang account',
         text: {
-            line_1: "If you have already paid for Minecraft, you could enter your Mojang account password below to bind it.",
+            line_1: 'If you have already paid for Minecraft, you could enter your Mojang account password below to bind it.',
             line_2: "Of course, we won't save your password.",
-            line_3: 'If you passed the verification, you will get <span id="m-score">0</span> score.'
+            line_3: 'After passed the verification, you will get a player with the same name as your premium Minecraft player. Besides, you will become a "Pro" user and get <span id="m-score">0</span> score.'
         },
         submit: 'Submit'
     },
@@ -13,8 +13,8 @@ blessing.i18n.mojang_verification = {
         title: 'Update UUID',
         text: {
             line_1: 'If you want to join the server which deployed Yggdrasil API using your premium Minecraft player name with your premium Minecraft UUID, you could click the "Update UUID" button below to update it.',
-            line_2: "<strong>Tip:</strong> If your premium Minecraft player has already set up skin on our website, it will use the skin set up on the website in the game; if not, the skin you set up on Mojang will be automatically used.",
-            line_3: "<strong>Note:</strong> Only need to update once, no need to repeat the operation."
+            line_2: '<strong>Tip:</strong> If your premium Minecraft player has already set up skin on our website, it will use the skin set up on the website in the game; if not, the skin you set up on Mojang will be automatically used.',
+            line_3: '<strong>Note:</strong> Only need to update once, no need to repeat the operation.'
         },
         button: 'Update UUID'
     }
