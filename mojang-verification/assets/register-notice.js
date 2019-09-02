829f66a95fb57e4215b7305efa9c8052deda7154
Pig Fang <g-plane@hotmail.com>
2019-09-02 19:06:55 +0800

    tweak

--- mojang-verification/assets/register-notice.js
+++ mojang-verification/assets/register-notice.js
@@ -1,5 +1,5 @@
 blessing.event.on('i18nLoaded', i18n => {
   i18n.auth['login-link'] += blessing.locale === 'zh_CN'
-  ? '。Mojang 正版用户可直接登录，无需注册。'
-  : ' You could log in using your Mojang account if you have paid for Minecraft.'
+    ? '。Mojang 正版用户可直接登录，无需注册。'
+    : ' You could log in using your Mojang account if you have paid for Minecraft.'
 })
