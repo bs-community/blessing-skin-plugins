c8ec9a4ee1120bb77a85a47d63b7c43ee93d0dda
Pig Fang <g-plane@hotmail.com>
2019-12-06 10:35:03 +0800

    [password-transition] tweak views

---
+++ password-algorithm-transition/src/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GPlane\PasswordTransition;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class Configuration
+{
+    public function render()
+    {
+        $users = User::select(['password'])->get();
+        $count = User::count();
+
+        $info = $users->groupBy(function ($user) {
+            $password = $user->password;
+            if (Str::startsWith($password, '$2y')) {
+                return 'Bcrypt';
+            } else {
+                $length = strlen($password);
+                if ($length === 128) {
+                    return 'SHA512';
+                } elseif ($length === 64) {
+                    return 'SHA256';
+                } elseif ($length === 32) {
+                    return 'MD5';
+                }
+            }
+        })->map(function ($users, $algName) use ($count) {
+            $total = count($users);
+            $percentage = $total / $count * 100;
+            return $algName.': '.$total.' 位用户 ('.$percentage.'%)';
+        });
+
+        return view('GPlane\\PasswordTransition::config', compact('info'));
+    }
+}
