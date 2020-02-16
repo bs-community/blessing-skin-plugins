43024c44062c4f79053c1ad86ed1245f76c54aaa
Steven Qiu <littleqiu@littleservice.cn>
2020-02-16 08:50:29 +0800

    [eruda] improve code

--- eruda/bootstrap.php
+++ eruda/bootstrap.php
@@ -5,9 +5,10 @@ use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Http\Request;
 
 return function (Dispatcher $events, Request $request) {
-    if($request->has('eruda')) {
-        $events->listen(App\Events\RenderingHeader::class, function($event) {
-            $event->addContent('<script src="https://cdn.jsdelivr.net/npm/eruda"></script><script>eruda.init();</script>');
+    if ($request->has('eruda')) {
+        Hook::addScriptFileToPage("https://cdn.jsdelivr.net/npm/eruda");
+        $events->listen(App\Events\RenderingFooter::class, function($event) {
+            $event->addContent('<script>eruda.init();</script>');
         });
     }
 }
