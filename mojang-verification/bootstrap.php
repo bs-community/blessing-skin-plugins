e6a1e125c739b4f9eee0ab32deb7ae36cb94e31d
Pig Fang <g-plane@hotmail.com>
2020-03-03 17:16:34 +0800

    [mojang] add missing badge

--- mojang-verification/bootstrap.php
+++ mojang-verification/bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Services\Hook;
+use Blessing\Filter;
 use GPlane\Mojang\Listeners;
+use GPlane\Mojang\MojangVerification;
 use Illuminate\Contracts\Events\Dispatcher;
 
 require __DIR__.'/src/helpers.php';
 
-return function (Dispatcher $events) {
+return function (Dispatcher $events, Filter $filter) {
     View::composer('GPlane\Mojang::bind', function ($view) {
         $view->with('score', option('mojang_verification_score_award', 0));
     });
@@ -18,12 +20,20 @@ return function (Dispatcher $events) {
         Listeners\OnAuthenticated::class
     );
 
+    $filter->add('user_badges', function ($badges, $user) {
+        if (MojangVerification::where('user_id', $user->uid)->count() == 1) {
+            $badges[] = ['text' => trans('GPlane\Mojang::general.pro'), 'color' => 'purple'];
+        }
+
+        return $badges;
+    });
+
     Hook::addScriptFileToPage(
         plugin_assets('mojang-verification', 'register-notice.js'),
         ['auth/register']
     );
 
-    Hook::addRoute(function ($router) {
+    Hook::addRoute(function () {
         Route::prefix('mojang')
             ->middleware(['web', 'auth'])
             ->namespace('GPlane\Mojang')
