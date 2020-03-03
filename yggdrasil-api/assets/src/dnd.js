0975e80c205ac3f3cc785683ad7f06528efa1b8d
Asnxthaony <asnxthaony@outlook.com>
2020-03-03 17:58:52 +0800

    Fix dnd card

--- yggdrasil-api/assets/src/dnd.js
+++ yggdrasil-api/assets/src/dnd.js
@@ -6,9 +6,7 @@ clipboard.on('error', () => alert('无法访问剪贴板，请手动复制。'))
 
 document.body.addEventListener('dragstart', e => {
   if (e.target.id === 'dnd-button') {
-    const uri =
-      'authlib-injector:yggdrasil-server:' +
-      encodeURIComponent(yggdrasilApiRoot)
+    const uri = 'authlib-injector:yggdrasil-server:' + e.target.dataset.clipboardText
 
     e.dataTransfer.setData('text/plain', uri)
     e.dataTransfer.dropEffect = 'copy'
