8090138008791c36b439fa0409d7677afcda21f0
Pig Fang <g-plane@hotmail.com>
2020-04-23 10:11:02 +0800

    [usm] tweak

--- usm-api/bootstrap.php
+++ usm-api/bootstrap.php
@@ -4,6 +4,6 @@ use App\Services\Hook;
 
 return function () {
     Hook::addRoute(function () {
-        Route::any('/usm/{player}.json', 'Blessing\Usm\ProfileController@json');
+        Route::get('/usm/{player}.json', 'Blessing\Usm\ProfileController@json');
     });
 };
