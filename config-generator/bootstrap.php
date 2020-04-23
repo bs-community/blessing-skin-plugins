50bb275b3cc3f3354f1d8a50cd2ce94eb32404e7
Pig Fang <g-plane@hotmail.com>
2020-04-23 10:24:51 +0800

    [conf-generator] tweak routes

--- config-generator/bootstrap.php
+++ config-generator/bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 
 use App\Services\Hook;
-use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
+return function () {
     Hook::addMenuItem('user', 3, [
         'title' => 'Blessing\ConfigGenerator::config.generate-config',
         'link'  => 'user/config',
         'icon'  => 'fa-book'
     ]);
 
-    Hook::addRoute(function ($router) {
-        $router->get(
+    Hook::addRoute(function () {
+        Route::get(
             '/user/config',
             'Blessing\ConfigGenerator\Controller@generate'
         )->middleware(['web', 'auth']);
