52ace71051acfbeef6f39dac5d637fbcae7a048a
Pig Fang <g-plane@hotmail.com>
2020-06-11 11:50:19 +0800

    [oauth-google] init

---
+++ oauth-google/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+use Blessing\Filter;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Collection;
+
+return function (Dispatcher $events, Filter $filter) {
+    $events->listen(
+        'SocialiteProviders\Manager\SocialiteWasCalled',
+        'SocialiteProviders\Google\GoogleExtendSocialite@handle'
+    );
+
+    config(['services.github' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ]]);
+
+    $filter->add('oauth_providers', function (Collection $providers) {
+        $providers->put('google', [
+            'icon' => 'google',
+            'displayName' => 'Google',
+        ]);
+
+        return $providers;
+    });
+};
