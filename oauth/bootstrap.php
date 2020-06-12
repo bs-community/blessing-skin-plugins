863e669c55f957d55a1af8f9c970e251ef803ed0
Pig Fang <g-plane@hotmail.com>
2020-06-12 22:40:02 +0800

    [oauth] fix

--- oauth/bootstrap.php
+++ oauth/bootstrap.php
@@ -5,6 +5,8 @@ use Blessing\Filter;
 use Illuminate\Support\Facades\View;
 
 return function (Filter $filter) {
+    app()->register(SocialiteProviders\Manager\ServiceProvider::class);
+
     View::composer('Blessing\OAuthCore::providers', function ($view) use ($filter) {
         $providers = $filter->apply('oauth_providers', collect());
 
@@ -27,6 +29,7 @@ return function (Filter $filter) {
     Hook::addRoute(function () {
         Route::prefix('auth/login')
             ->namespace('Blessing\OAuthCore')
+            ->middleware(['web', 'guest'])
             ->group(function () {
                 Route::get('{driver}', 'AuthController@login');
                 Route::get('{driver}/callback', 'AuthController@callback');
