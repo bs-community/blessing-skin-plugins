3e76cc1ed39e26fca3a36271417527de5467a013
Pig Fang <g-plane@hotmail.com>
2020-06-20 09:45:11 +0800

    [disable-registration] fix home page button

--- disable-registration/assets/modifyButton.ts
+++ disable-registration/assets/modifyButton.ts
@@ -1,6 +1,6 @@
 {
   const button = document.querySelector<HTMLAnchorElement>('.main-button')
-  if (button) {
+  if (button && button.href.endsWith('/auth/register')) {
     button.textContent = trans('auth.login')
     const url = new URL(blessing.base_url)
     url.pathname = '/auth/login'
