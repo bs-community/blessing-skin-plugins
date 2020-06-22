b1ca8c8ba213625d04fcdafa3f144e6fef4474be
Pig Fang <g-plane@hotmail.com>
2020-06-22 10:01:09 +0800

    [mojang-verification] tweak i18n

--- mojang-verification/assets/register-notice.ts
+++ mojang-verification/assets/register-notice.ts
@@ -1,10 +1,7 @@
 blessing.event.on('mounted', () => {
   const callout = document.createElement('div')
   callout.className = 'callout callout-info'
-  callout.textContent =
-    blessing.locale === 'zh_CN'
-      ? 'Mojang 正版用户可直接登录，无需注册。'
-      : 'You could log in using your Mojang account if you have paid for Minecraft.'
+  callout.textContent = trans('mojang-verification.registration.notice')
 
   document.querySelector('.login-card-body')?.appendChild(callout)
 })
