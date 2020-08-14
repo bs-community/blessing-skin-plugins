a4161f4e56f77ce1cbf24d01858c8946f0e7e146
Pig Fang <g-plane@hotmail.com>
2020-08-14 11:58:46 +0800

    add description editor at upload page

--- plugins/texture-description/bootstrap.php
+++ plugins/texture-description/bootstrap.php
@@ -2,15 +2,18 @@
 
 namespace Blessing\TextureDesc;
 
+use App\Events\RenderingFooter;
 use App\Models\Texture;
 use App\Models\User;
 use App\Services\Hook;
 use App\Services\Plugin;
 use Blessing\Filter;
+use Blessing\TextureDesc\Listeners\AddDescriptionOnUpload;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 
-return function (Filter $filter, Plugin $plugin) {
+return function (Filter $filter, Plugin $plugin, Dispatcher $events) {
     Hook::addRoute(function () {
         Route::namespace('Blessing\TextureDesc\Controllers')
             ->group(function () {
@@ -24,12 +27,23 @@ return function (Filter $filter, Plugin $plugin) {
             });
     });
 
+    $events->listen('texture.uploaded', AddDescriptionOnUpload::class);
+
     $filter->add('grid:skinlib.show', function (array $grid) {
         $grid['widgets'][0][0][] = 'Blessing\TextureDesc::description';
 
         return $grid;
     });
     Hook::addScriptFileToPage($plugin->assets('Description.js'), ['skinlib/show/*']);
+
+    $events->listen(RenderingFooter::class, function (RenderingFooter $event) {
+        if (request()->is('skinlib/upload')) {
+            $event->addContent(
+                '<input id="desc-limit" type="hidden" value="'.(int) option('textures_desc_limit', 0).'">'
+            );
+        }
+    });
+    Hook::addScriptFileToPage($plugin->assets('UploadEditor.js'), ['skinlib/upload']);
 
     View::composer('Blessing\TextureDesc::description', function ($view) {
         $view->with('max_length', option('textures_desc_limit', 0));
---
+++ plugins/texture-description/src/Listeners/AddDescriptionOnUpload.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Blessing\TextureDesc\Listeners;
+
+use App\Models\Texture;
+use Blessing\TextureDesc\Models\Description;
+
+class AddDescriptionOnUpload
+{
+    public function handle(Texture $texture)
+    {
+        Description::create([
+            'tid' => $texture->tid,
+            'desc' => request('description'),
+        ]);
+    }
+}
