dc32d1dc68da8e74cf5d34b9c11a0a7b3efa5e26
Pig Fang <g-plane@hotmail.com>
2020-08-16 19:19:39 +0800

    add

--- plugins/texture-description/assets/Description.tsx
+++ plugins/texture-description/assets/Description.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { fetch, t, notify } from 'blessing-skin'
+import './markdown.scss'
 
 const el = document.querySelector<HTMLDivElement>('#texture-description')
 
