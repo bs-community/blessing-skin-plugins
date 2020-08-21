d689fd023e1975cc25dccd22039890c91d177d78
Pig Fang <g-plane@hotmail.com>
2020-08-21 16:54:27 +0800

    [texture-description] refactor migration

--- plugins/texture-description/callbacks.php
+++ plugins/texture-description/callbacks.php
@@ -1,13 +1,17 @@
 <?php
 
-use Blessing\TextureDescription\Models\Description;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return [
     App\Events\PluginWasEnabled::class => function () {
-        if (!Schema::hasTable('textures_description')) {
+        if (Schema::hasTable('textures_desc')) {
+            Schema::table('textures_desc', function (Blueprint $table) {
+                $table->renameColumn('desc', 'description');
+                $table->rename('textures_description');
+            });
+        } elseif (!Schema::hasTable('textures_description')) {
             Schema::create('textures_description', function (Blueprint $table) {
                 $table->bigIncrements('id');
                 $table->bigInteger('tid')->unsigned()->unique();
@@ -15,22 +19,8 @@ return [
             });
         }
 
-        if (Schema::hasTable('textures_desc')) {
-            DB::table('textures_desc')
-                ->get()
-                ->each(function ($row) {
-                    Description::create([
-                        'tid' => $row->tid,
-                        'description' => $row->desc,
-                    ]);
-                });
-            Schema::dropIfExists('textures_desc');
-        }
-
-        $oldOption = option('textures_desc_limit');
-        if ($oldOption) {
-            option(['textures_description_limit' => $oldOption]);
-            DB::table('options')->where('option_name', 'textures_desc_limit')->delete();
-        }
+        DB::table('options')
+            ->where('option_name', 'textures_desc_limit')
+            ->update(['option_name' => 'textures_description_limit']);
     },
 ];
