d9824aaf9a8d4a39b65312d2af92bec7054737e0
Pig Fang <g-plane@hotmail.com>
2020-08-28 10:58:54 +0800

    [texture-description] fix style isn't applied

--- plugins/texture-description/assets/UploadEditor.svelte
+++ plugins/texture-description/assets/UploadEditor.svelte
@@ -3,18 +3,13 @@
   import { event, t } from 'blessing-skin'
 
   let description = ''
-  let maxLength = Infinity
+  export let maxLength = Infinity
 
   onMount(() => {
     const off = event.on('beforeFetch', (request: { data: FormData }) => {
       request.data.set('description', description)
     })
     onDestroy(off)
-  })
-
-  onMount(() => {
-    const input = document.querySelector<HTMLInputElement>('#description-limit')
-    maxLength = Number.parseInt(input?.value ?? '0') || Infinity
   })
 </script>
 
@@ -25,7 +20,7 @@
   rows="9"
   bind:value={description} />
 {#if description.length > maxLength}
-  <div className="alert alert-info mt-2">
+  <div class="alert alert-info mt-2">
     {t('texture-description.exceeded', { max: maxLength })}
   </div>
 {/if}
--- plugins/texture-description/assets/UploadEditor.ts
+++ plugins/texture-description/assets/UploadEditor.ts
@@ -2,6 +2,9 @@ import { event } from 'blessing-skin'
 import UploadEditor from './UploadEditor.svelte'
 
 event.on('mounted', () => {
+  const input = document.querySelector<HTMLInputElement>('#description-limit')
+  const maxLength = Number.parseInt(input?.value ?? '0') || Infinity
+
   const lastFormGroup = document.querySelector<HTMLDivElement>(
     '#file-input .form-group:nth-child(3)',
   )
@@ -9,6 +12,6 @@ event.on('mounted', () => {
     const formGroup = document.createElement('div')
     formGroup.className = 'form-group'
     lastFormGroup.after(formGroup)
-    new UploadEditor({ target: formGroup })
+    new UploadEditor({ target: formGroup, props: { maxLength } })
   }
 })
