23b3f14bebf942415629cb569422b4e8d0d560c3
Pig Fang <g-plane@hotmail.com>
2021-02-14 12:17:51 +0800

    remove "fix-v4-update" plugin

--- plugins/fix-v4-update/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use GuzzleHttp\Client;
-
-return function () {
-    app()->bind(App\Services\PackageManager::class, function () {
-        return new Blessing\FixV4Update\PackageManager(new Client());
-    });
-};
