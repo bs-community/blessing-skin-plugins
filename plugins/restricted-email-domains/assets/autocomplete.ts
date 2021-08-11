8f64dc2a2269683405ac5cfd92a3aa0e4d66301c
Pig Fang <g-plane@hotmail.com>
2021-08-11 23:20:31 +0800

    [restricted-email-domains] enhance email autocomplete
    close #119

---
+++ plugins/restricted-email-domains/assets/autocomplete.ts
@@ -0,0 +1,14 @@
+import { event } from 'blessing-skin'
+
+event.on('emailDomainsSuggestion', (domains: Set<string>) => {
+  const allowListJSON = document.querySelector('#allowed-email-domains')
+  if (!allowListJSON) {
+    return
+  }
+
+  const allowList: string[] = JSON.parse(allowListJSON.textContent ?? '[]')
+  if (allowList.length > 0) {
+    domains.clear()
+    allowList.forEach((domain) => domains.add(domain))
+  }
+})
--- plugins/restricted-email-domains/bootstrap.php
+++ plugins/restricted-email-domains/bootstrap.php
@@ -2,13 +2,34 @@
 
 namespace Blessing\RestrictedEmailDomains;
 
+use App\Events\RenderingFooter;
 use App\Services\Hook;
+use App\Services\Plugin;
 use Blessing\Filter;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Http\Request;
 use Route;
 
-return function (Filter $filter) {
+return function (Filter $filter, Dispatcher $events, Request $request, Plugin $plugin) {
     $filter->add('can_register', EmailFilter::class);
     $filter->add('user_can_edit_profile', EmailFilter::class);
+
+    $events->listen(RenderingFooter::class, function (RenderingFooter $event) use ($request) {
+        if (!$request->is('auth/*')) {
+            return;
+        }
+
+        $domains = option('restricted-email-domains.allow', '[]');
+        $event->addContent('<script type="application/json" id="allowed-email-domains">'.$domains.'</script>');
+    });
+
+    $filter->add('scripts', function ($scripts) use ($plugin, $request) {
+        if ($request->is('auth/*')) {
+            $scripts[] = ['src' => $plugin->assets('autocomplete.js'), 'crossorigin' => 'anonymous'];
+        }
+
+        return $scripts;
+    });
 
     Hook::addRoute(function () {
         Route::namespace('Blessing\RestrictedEmailDomains')
