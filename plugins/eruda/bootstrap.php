86272bb9e796419a933afd17685392f62215b09a
Pig Fang <g-plane@hotmail.com>
2022-06-10 15:55:16 +0800

    [eruda] fix CDN link

--- plugins/eruda/bootstrap.php
+++ plugins/eruda/bootstrap.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 return function (Dispatcher $events, Request $request) {
     if ($request->has('eruda')) {
-        Hook::addScriptFileToPage('https://cdn.jsdelivr.net/npm/eruda');
+        Hook::addScriptFileToPage('https://unpkg.com/eruda');
         $events->listen(App\Events\RenderingFooter::class, function ($event) {
             $event->addContent('<script>eruda.init();</script>');
         });
