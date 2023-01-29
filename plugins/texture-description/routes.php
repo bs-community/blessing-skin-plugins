7bca674bcd92c1e6e7c76d5ee5c94d3d99ce5b35
Pig Fang <g-plane@hotmail.com>
2023-01-29 11:57:06 +0800

    [texture-description] update middleware

--- plugins/texture-description/routes.php
+++ plugins/texture-description/routes.php
@@ -1,14 +1,16 @@
 <?php
 
+use Illuminate\Routing\Middleware\SubstituteBindings;
+
 Route::prefix('texture/{texture}/description')
-    ->middleware(['web', 'bindings'])
+    ->middleware(['web', SubstituteBindings::class])
     ->group(function () {
         Route::get('', 'DescriptionController@read');
         Route::put('', 'DescriptionController@update')->middleware(['auth']);
     });
 
 Route::prefix('api/textures/{texture}/description')
-    ->middleware(['api', 'throttle:60,1', 'bindings'])
+    ->middleware(['api', 'throttle:60,1', SubstituteBindings::class])
     ->group(function () {
         Route::get('', 'DescriptionController@read');
         Route::put('', 'DescriptionController@update')->middleware(['auth']);
